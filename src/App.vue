cf173aa2195a8483084a51b024b83fa069a4df9e
Michael Urquhart <michaelurquhart@hotmail.com>
2019-06-26 17:35:40 +0100

    MVP complete.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
+    <h1>Countries</h1>
+    <div class="main-container">
+      <countries-list :countries='countries'></countries-list>
+      <country-detail :country='selectedCountry'></country-detail>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountriesList from './components/CountriesList.vue'
+import CountryDetail from './components/CountryDetail.vue'
+import {eventBus} from './main.js'
 
 export default {
   name: 'app',
+  data(){
+    return{
+      countries: [],
+      selectedCountry: null
+    }
+  },
+  mounted(){
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(countries => this.countries = countries)
+
+    eventBus.$on('country-select', (country) => {
+      this.selectedCountry = country;
+    })
+
+  },
   components: {
-    HelloWorld
+    "countries-list": CountriesList,
+    "country-detail": CountryDetail
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+
+  <div v-if='country'>
+    <h3>{{country.name}}</h3>
+    <p>capital: {{ country.capital}}</p>
+    <p>population: {{ country.population}}</p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'country-detail',
+  props: ['country']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
