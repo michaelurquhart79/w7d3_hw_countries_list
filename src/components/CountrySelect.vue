515df6cf7308fe8d4bd60982a01ef2593ec84220
Michael Urquhart <michaelurquhart@hotmail.com>
2019-06-26 21:48:09 +0100

    Extensions complete.

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+
+  <select v-on:change="handleSelect" v-model="selected">
+    <option disabled value="">Please select one</option>
+    <option v-for="(country, index) in countries" :value="index">{{country.name}}</option>
+  </select>
+
+
+
+</template>
+
+<script>
+
+import { eventBus} from '../main.js'
+
+export default {
+  name: "country-select",
+  data(){
+    return{
+      selected: null,
+    }
+  },
+  props: ['countries'],
+  methods: {
+    handleSelect(){
+      eventBus.$emit('country-selected', this.selected);
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
